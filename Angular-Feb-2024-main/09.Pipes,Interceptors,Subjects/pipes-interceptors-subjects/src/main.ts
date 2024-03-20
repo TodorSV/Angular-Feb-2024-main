b771ae6cc6702de3918541a4e96e620496aeddfe
Todor Slavov <144543159+TodorSV@users.noreply.github.com>
2024-03-20 20:05:37 +0200

    Add navigation bar styles and Subjects demos.

--- Angular-Feb-2024-main/09.Pipes,Interceptors,Subjects/pipes-interceptors-subjects/src/main.ts
+++ Angular-Feb-2024-main/09.Pipes,Interceptors,Subjects/pipes-interceptors-subjects/src/main.ts
@@ -1,7 +1,38 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { Observable, map } from 'rxjs';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+  // Refresher
+
+  const p = new Promise((resolve, reject) => {
+    resolve(200);
+  });
+
+  p.then(console.log);
+
+  const o$ = new Observable<number>((obs) => {
+    obs.next(101);
+    obs.next(102);
+    obs.next(103);
+
+    obs.error(new Error('Something went wrong'));
+
+    return () => {
+      // Clean up
+
+    };
+  });
+
+ 
+ 
+  o$.pipe(map((n) => n + 1 )).subscribe({
+    next: console.log,
+    error: (err) => console.error('Error form subscribe', err),
+  });
+
+ 
