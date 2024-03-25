ad7ae8248a015498d2f0c9d96390d37f887146bc
Todor Slavov <144543159+TodorSV@users.noreply.github.com>
2024-03-25 13:16:51 +0200

    Login updates.

---
+++ Angular-Feb-2024-main/10.Workshop Pipes, Interceptors and Subjects/workshop-interceptors/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { UserModule } from './user/user.module';
+import { ThemeModule } from './theme/theme.module';
+import { ErrorComponent } from './error/error.component';
+import { appInterceptorProvider } from './app.interceptor';
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent, ErrorComponent],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    SharedModule,
+    HttpClientModule,
+    UserModule,
+    ThemeModule,
+    AppRoutingModule,
+  ],
+  providers: [appInterceptorProvider],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
